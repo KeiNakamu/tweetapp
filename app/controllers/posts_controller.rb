c37e165a3a6b27efc97fb48b40bef900f1e0cc1e
Kei <kei.n140802@gmail.com>
2022-06-07 15:30:55 +0900

    new(新規画面)を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,29 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+      @post = Post.new(post_params)
+      if params[:back]
+        render :new
+      else
+        if @post.save
+          redirect_to posts_path, notice: "つぶやきました！"
+        else
+          render :new
+        end
+      end
+    end
+
+    private
+
+  def post_params
+  params.require(:post).permit(:id, :content)
+  end
+
 end
